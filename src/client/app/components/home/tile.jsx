753ff0c5f7843cfc9fbfc893b4b61a6cdd68945e
James Owens <jamesmarshallowens@gmail.com>
2017-06-02 22:43:26 -0400

    Basic tile formatting

--- src/client/app/components/home/tile.jsx
+++ src/client/app/components/home/tile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import getShadowForLevel from '../../utilities/shadowHelper.js';
 
 class Tile extends React.Component {
   render() {
@@ -9,11 +10,31 @@ class Tile extends React.Component {
       justifyContent: 'center',
       alignItems: 'center',
       width: '100%',
-      height: '200px',
+      padding: '10px',
       backgroundColor: 'white',
+      boxShadow: getShadowForLevel(1),
+    }
+    const imgStyle = {
+      borderRadius: '50%',
+      marginRight: '15px',
+    }
+    const textBoxStyle = {
+      minWidth: '150px',
+      maxWidth: '75%',
+      flexGrow: 1,
+      marginTop: '5px',
+      paddingRight: '5px',
+      fontSize: '16pt',
     }
     return (
-      <div style={styles} />
+      <div style={styles}>
+        <img style={imgStyle} src="http://lorempixel.com/256/256" />
+        <div style={textBoxStyle}>
+          <h2>hi</h2>
+          <div>This is the body</div>
+          <div style={{marginTop: '5px'}}>See More</div>
+        </div>
+      </div>
     );
   }
 }
