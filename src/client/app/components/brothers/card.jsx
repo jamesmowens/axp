a761b2a133d4f05f23a6355fd5b74d6c60ecc14f
James Owens <jamesmarshallowens@gmail.com>
2017-07-11 19:45:52 -0400

    linked the card box to the data

--- src/client/app/components/brothers/card.jsx
+++ src/client/app/components/brothers/card.jsx
@@ -1,42 +1,57 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import getShadowForLevel from '../../utilities/shadowHelper';
 import lorem from '../../utilities/strings';
+
+function getStyles() {
+  return {
+    container: {
+      backgroundColor: 'white',
+      width: '300px',
+      height: '350px',
+      boxShadow: getShadowForLevel(1),
+      borderRadius: '5%',
+      textAlign: 'center',
+      padding: '10px',
+      marginLeft: '10px',
+      marginBottom: '10px',
+    },
+    img: {
+      width: '128px',
+      height: '128px',
+      borderRadius: '50%',
+    },
+    name: {
+      fontSize: '14pt',
+    },
+    body: {
+      fontSize: '9pt',
+      textAlign: 'left',
+    },
+  };
+}
 
 export default class Card extends React.Component { // eslint-disable-line
 
   render() {
-    const styles = {
-      container: {
-        backgroundColor: 'white',
-        width: '300',
-        height: '350',
-        boxShadow: getShadowForLevel(1),
-        borderRadius: '5%',
-        textAlign: 'center',
-        padding: '10px',
-        marginLeft: '10px',
-        marginBottom: '10px',
-      },
-      img: {
-        width: '128',
-        height: '128',
-        borderRadius: '50%',
-      },
-      name: {
-        fontSize: '14pt',
-      },
-      body: {
-        fontSize: '9pt',
-        textAlign: 'left',
-      },
-    };
+    const styles = getStyles();
+
     return (
       <div style={styles.container}>
         <img style={styles.img} alt="" src="http://lorempixel.com/256/256" />
-        <p style={styles.name}>James Owens | Member at Large</p>
-        <p style={styles.body}>{lorem}</p>
+        <p style={styles.name}>{this.props.name} | {this.props.position}</p>
+        <p style={styles.body}>{this.props.bio}</p>
       </div>
     );
   }
 }
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  bio: PropTypes.string.isRequired,
+};
+
+Card.defaultProps = {
+  bio: lorem,
+};
--- src/client/app/components/brothers/cardBox.jsx
+++ src/client/app/components/brothers/cardBox.jsx
@@ -2,8 +2,44 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import getShadowForLevel from '../../utilities/shadowHelper';
 import Card from './card';
+import getBrothers from '../../data/brothers';
+
+function getStyles() {
+  return {
+    fieldCont: {
+      width: '75%',
+    },
+    searchCont: {
+      width: '100%',
+      // height: '100px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      paddingBottom: '10px',
+      boxShadow: getShadowForLevel(1),
+    },
+    boxCont: {
+      display: 'flex',
+      margin: '10px',
+      flexWrap: 'wrap',
+    },
+  };
+}
+
 
 export default class CardBox extends React.Component {
+
+  static createCards() {
+    const brothers = getBrothers();
+    return brothers.map(brother =>
+      (<Card
+        name={`${brother.first} ${brother.last}`}
+        position={brother.position ? brother.position : 'Brother'}
+        key={brother.scroll}
+      />));
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -12,32 +48,15 @@ export default class CardBox extends React.Component {
   }
 
   render() {
-    const styles = {
-      searchCont: {
-        width: '100%',
-        // height: '100px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        paddingBottom: '10px',
-        boxShadow: getShadowForLevel(1),
-      },
-      boxCont: {
-        display: 'flex',
-        margin: '10px',
-        flexWrap: 'wrap',
-      }
-    };
+    const styles = getStyles();
+    const { searchCont, fieldCont, boxCont } = styles;
     return (
       <div>
-        <div style={styles.searchCont}>
-          <TextField floatingLabelText="Search" />
+        <div style={searchCont}>
+          <div style={fieldCont}><TextField floatingLabelText="Search" fullWidth /></div>
         </div>
-        <div style={styles.boxCont}>
-          <Card />
-          <Card />
-          <Card />
+        <div style={boxCont}>
+          {CardBox.createCards()}
         </div>
       </div>
     );
